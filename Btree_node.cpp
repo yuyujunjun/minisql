359729d3365ed8549602411e3e028cd8ad660473
yunuohanxue <fcsx1tf@163.com>
2017-05-11 13:03:50 +0800

    preparing to try btree

--- Btree_node.cpp
+++ Btree_node.cpp
@@ -8,9 +8,17 @@ Btree_node::Btree_node(int type, int typelength, string indexname)
 	keynum = (BlockSize - BlockHeadSize)/(attri_length+offset_length);
 	filename = "index_" + indexname + ".record";
 	this->block = BM.GetBlock(filename);
-	parent = child = NULL;
+	parent  = NULL;
+	
 }
-
+Btree_node::Btree_node(Block* block, int type, int typelength, string indexname) {//已经有块了，从缓存中把块生成结点，再生成b+树
+	int attri_length = GetAttributeLength(type, typelength);
+	int offset_length = GetAttributeLength(INT);
+	keynum = (BlockSize - BlockHeadSize) / (attri_length + offset_length);
+	filename = "index_" + indexname + ".record";
+	this->block = block;
+	parent =  NULL;
+}
 
 Btree_node::~Btree_node()
 {
--- Btree_node.h
+++ Btree_node.h
@@ -8,15 +8,24 @@ class Btree_node
 {
 public:
 	Btree_node(int type,int typelength,string indexname);//建立索引的属性名，该属性的长度，该属性的名字
+	Btree_node(Block* block,int type,int typelength,string indexname);//从文件打开时，对建立好的索引的块生成结点
 	int keynum;//键值的多少,也是最大degree
+	//树相关结构
 	Btree_node *parent;
-	Btree_node *child;
+	vector<Btree_node *>child;
+	Btree_node *nextsilbling;
+	//信息结构
 	vector<int> blocknum;//在文件的第几个块
 	vector<int> attr_int;//整型的属性
 	vector<float> attr_float;
 	vector<string> attr_char;
+	//存储的结构
 	Block* block;//该块数据存储的地方
 	string filename;//该块数据储存地方的名字
 	~Btree_node();
 };
 
+class Bptree {
+	Btree_node* node;
+
+};
